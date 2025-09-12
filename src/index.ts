cbbc343379b1c1154ea66965f6d388e8dcaaaf54
Tarek Jaman Labu <tarekjamanlabu515@gmail.com>
2025-09-13 00:16:48 +0600

    Add dependencies of graphql

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import express, { Request, Response } from 'express';
+import "dotenv/config"
+import express, { Request, Response } from "express"
+import { clerkMiddleware, requireAuth } from "@clerk/express"
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.get('/', (req: Request, res: Response) => {
-    res.json({ message: 'Hello World!' });
-});
-app.get('/msg', (req: Request, res: Response) => {
-    res.json({ message: 'Hello msg!' });
+app.use(clerkMiddleware())
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello World!" })
+})
+app.get("/msg", requireAuth(), (req: Request, res: Response) => {
+  res.json({ message: "Hello msg!" })
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
